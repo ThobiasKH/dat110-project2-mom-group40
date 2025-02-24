7cf66da58767255d34ac81fbfb918edfd455fbba
ThobiasKH <thobiashoivik@gmail.com>
2025-02-24 22:05:12 +0100

    Task C

--- src/main/java/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/main/java/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -1,9 +1,6 @@
 package no.hvl.dat110.iotsystem;
 
 import no.hvl.dat110.client.Client;
-import no.hvl.dat110.messages.Message;
-import no.hvl.dat110.messages.PublishMsg;
-import no.hvl.dat110.common.TODO;
 
 public class DisplayDevice {
 	
@@ -16,19 +13,29 @@ public class DisplayDevice {
 		// TODO - START
 				
 		// create a client object and use it to
-		
+		Client client = new Client("display", Common.BROKERHOST, Common.BROKERPORT);
+
 		// - connect to the broker - use "display" as the username
+		client.connect();
 		// - create the temperature topic on the broker
+		client.createTopic(Common.TEMPTOPIC);
 		// - subscribe to the topic
+		client.subscribe(Common.TEMPTOPIC);
 		// - receive messages on the topic
+		for (int i = 0; i < COUNT; i++) {
+			String message = client.receive().toString();
+			System.out.println("DISPLAY: " + message);
+		}
 		// - unsubscribe from the topic
+		client.unsubscribe(Common.TEMPTOPIC);
 		// - disconnect from the broker
+		client.disconnect();
 		
 		// TODO - END
 		
 		System.out.println("Display stopping ... ");
 		
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 		
 	}
 }
--- src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -1,7 +1,6 @@
 package no.hvl.dat110.iotsystem;
 
 import no.hvl.dat110.client.Client;
-import no.hvl.dat110.common.TODO;
 
 public class TemperatureDevice {
 
@@ -14,9 +13,25 @@ public class TemperatureDevice {
 
 		// TODO - start
 
+		System.out.println("Temperature device start");
 		// create a client object and use it to
+		Client client = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
 
 		// - connect to the broker - user "sensor" as the user name
+		client.connect();
+		//Random random = new Random();
+		for (int i = 0; i < COUNT; i++) {
+			int temp = sn.read();
+			System.out.println("READING: " + temp);
+			client.publish(Common.TEMPTOPIC, String.valueOf(temp));
+
+			try {
+				Thread.sleep(1000);		
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 		// - publish the temperature(s)
 		// - disconnect from the broker
 
@@ -24,7 +39,7 @@ public class TemperatureDevice {
 
 		System.out.println("Temperature device stopping ... ");
 
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
